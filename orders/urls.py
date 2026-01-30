ac38a431f7e4739df97193b9da8e4ff45dbec140
GamaNoty <noga.ma.2023@skola.ssps.cz>
2026-01-30 21:27:34 +0100

    Add cancel order for customers and archived for staff

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('zamestnanec/', views.staff_dashboard, name='staff_dashboard'),
     
     path('order/<int:order_id>/<str:action>/', views.order_action, name='order_action'),
+    path('cancel-order/<int:order_id>/', views.customer_cancel_order, name='customer_cancel_order'),
+    path('archive-order/<int:order_id>/', views.archive_order, name='archive_order')
 ]
